09059ef7207cbc574095017d18561ae6576c70de
Hafizuddin Sharif <hafizuddin.sharif@cimb.com>
2024-02-25 14:24:48 +0800

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import WithPWA from "next-pwa";
+
+const withPWA = WithPWA({
+  dest: "public",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
